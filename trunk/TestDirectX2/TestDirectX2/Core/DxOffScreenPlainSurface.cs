3b403360eaed58a486ab3f9077f29754b1982914
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@76baa962-0c3b-e8cc-c4fd-2fd9930c05a7>
2012-07-22 04:32:45 +0000

    Them DxOffScreenPlainSurface.cs va DxScreen.cs

---
+++ trunk/TestDirectX2/TestDirectX2/Core/DxOffScreenPlainSurface.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.DirectX.DirectDraw;
+
+namespace TestDirectX2.Core
+{
+    class DxOffScreenPlainSurface
+    {
+        private Surface _surface;
+
+        public Surface Surface
+        {
+            get { return _surface; }
+            set { _surface = value; }
+        }
+
+        public DxOffScreenPlainSurface(DxInitGraphics graphics, int width, int height)
+        {
+            SurfaceDescription _desc = new SurfaceDescription();
+            _desc.SurfaceCaps.OffScreenPlain = true;
+            _desc.Width = width;
+            _desc.Height = height;
+            _surface = new Surface(_desc, graphics.GraphicsDevice);
+        }
+
+
+    }
+}
