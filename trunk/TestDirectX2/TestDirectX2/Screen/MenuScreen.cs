bcfe7565dbae0438432b663b985e5ea70198fa8a
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@76baa962-0c3b-e8cc-c4fd-2fd9930c05a7>
2012-08-05 02:20:06 +0000

    Screens In Game

---
+++ trunk/TestDirectX2/TestDirectX2/Screen/MenuScreen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestDirectX2.Core;
+
+namespace TestDirectX2
+{
+    public class MenuScreen:DxScreen
+    {
+
+
+    }
+}
