269e1ad09ffb59ad34d383a0f42bee67f09f2ff5
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@76baa962-0c3b-e8cc-c4fd-2fd9930c05a7>
2012-08-05 02:29:20 +0000

    Class Character and GameManager

---
+++ TestDirectX2/TestDirectX2/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestDirectX2.Core;
+
+namespace TestDirectX2
+{
+    public class Character
+    {
+        protected DxInitSprite _image;
+        protected int _hp;
+        protected int _damage;
+        protected int _power;
+       
+    }
+}
---
+++ TestDirectX2/TestDirectX2/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestDirectX2
+{
+    public class GameManager
+    {
+        ScreenManager _scrManager;
+
+    }
+}
