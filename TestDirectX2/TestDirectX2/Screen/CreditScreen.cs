bf144fe0109df7f60944b843b0c4d45cd0e5e2e7
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@76baa962-0c3b-e8cc-c4fd-2fd9930c05a7>
2012-08-05 02:20:06 +0000

    Screens In Game

---
+++ TestDirectX2/TestDirectX2/Screen/CreditScreen.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestDirectX2.Core;
+
+namespace TestDirectX2.Screen
+{
+    public class CreditScreen:DxScreen
+    {
+
+    }
+}
---
+++ TestDirectX2/TestDirectX2/Screen/SplashScreen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using TestDirectX2.Core;
+using Microsoft.DirectX.DirectDraw;
+
+namespace TestDirectX2
+{
+    public class SplashScreen:DxScreen
+
+    {
+        private double _ellapsedTime = 0;
+        private double _liveTime = 0;
+
+        public bool IsDone
+        {
+            get { return _ellapsedTime >= _liveTime; }
+        }
+
+        public SplashScreen(DxInitGraphics graphics, Point location, Size size, double liveTime):
+            base(graphics, location, size)
+        {
+            this._liveTime = liveTime;
+            Initialize();
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            _surface.ColorFill(Color.FromArgb(0, 255, 0, 255));
+        }
+
+        public override void Update(double deltaTime)
+        {
+            _ellapsedTime += deltaTime;
+            base.Update(deltaTime);
+        }
+
+        public override void Draw(double deltaTime)
+        {
+            _graphics.SecondarySurface.DrawFast(_location.X, _location.Y, _surface, DrawFastFlags.Wait);
+            base.Draw(deltaTime);
+        }
+    }
+}
