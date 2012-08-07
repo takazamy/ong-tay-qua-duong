34ad55b1c25767306853ee226e1ce20a41462adc
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@76baa962-0c3b-e8cc-c4fd-2fd9930c05a7>
2012-08-07 14:29:23 +0000

    Tạo GameManager và splashScreen

--- TestDirectX2/TestDirectX2/GameManager.cs
+++ TestDirectX2/TestDirectX2/GameManager.cs
@@ -2,12 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.DirectX.DirectInput;
 
 namespace TestDirectX2
 {
     public class GameManager
     {
-        ScreenManager _scrManager;
+        
 
+        private DxInitGraphics _graphics;
+        private Control _renderTarget;
+
+        public Control RenderTarget
+        {
+            get { return _renderTarget; }
+            set { _renderTarget = value; }
+        }
+
+        public DxInitGraphics Graphics
+        {
+            get { return _graphics; }
+            set { _graphics = value; }
+        }
+
+        public ScreenManager _scrManager;      
+
+        public GameManager(Control parent,DxInitGraphics graphics )
+        {
+            _renderTarget = parent;
+            _graphics = graphics;
+            _scrManager = new ScreenManager();
+            Initialized();
+           
+            _scrManager.PlayScreen(0);
+        }
+
+        public void Initialized()
+        {
+            _scrManager.Append(new SplashScreen(_scrManager, _graphics, Point.Empty,
+              new Size(800, 600),
+              5000));
+            _scrManager.Append(new MenuScreen(_scrManager,_graphics,Point.Empty,
+                 new Size(800, 600)));
+        }
+        public void Update(double deltaTime, KeyboardState keyState, MouseState mouseState)
+        {
+            _scrManager.Update(deltaTime, keyState, mouseState);
+        }
+
+        public void Draw(DxInitGraphics graphics, double deltaTime)
+        {
+            _scrManager.Draw(graphics, deltaTime);
+        }
     }
 }
