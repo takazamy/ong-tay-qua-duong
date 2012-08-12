d1732dc9096c120dc6cef9e8f98700aea9130938
tuquoctrungx@gmail.com <tuquoctrungx@gmail.com@76baa962-0c3b-e8cc-c4fd-2fd9930c05a7>
2012-08-12 03:34:36 +0000

    Update Enemy

---
+++ trunk/TestDirectX2/TestDirectX2/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace TestDirectX2
+{
+    public class Player:Character
+    {
+        public override void Update(double deltaTime, Microsoft.DirectX.DirectInput.KeyboardState keyState, Microsoft.DirectX.DirectInput.MouseState mouseState)
+        {
+            base.Update(deltaTime, keyState, mouseState);
+        }
+        public override void Move(Microsoft.DirectX.DirectInput.KeyboardState keyState)
+        {
+            base.Move(keyState);
+        }
+    }
+}
