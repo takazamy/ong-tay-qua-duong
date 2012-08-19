601f831a7e4c59eb981eae2b1bfccb800a4ee4f5
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@76baa962-0c3b-e8cc-c4fd-2fd9930c05a7>
2012-08-19 15:45:18 +0000

    Cập nhật các Class Character để thực hiện Animation khi di chuyển và đứng lại(đã vẽ được animation cho các enemy). Cập nhật MapLoader để Load các data trong Xml và tạo nên một danh sách các enemy

--- TestDirectX2/TestDirectX2/Enemy.cs
+++ TestDirectX2/TestDirectX2/Enemy.cs
@@ -11,14 +11,41 @@ using Microsoft.DirectX.DirectInput;
 
 namespace TestDirectX2
 {
+     
     public class Enemy:Character
     {
+
+
+        AnimationKey _moveR;
+        AnimationKey _moveL;
+        AnimationKey _stay;
         public Enemy(float x, float y, int hp, int damage, int power, int moveSpeed, DxInitSprite sprite, int direction) :
             base(x, y, hp,damage, power, moveSpeed, sprite, direction)
         {
-           
-        }
+            _moveR._range = new List<int>();
+            _moveR._range.Add(1);
+            _moveR._range.Add(2);
+            _moveR._range.Add(3);
+            _moveR._range.Add(4);
+            _moveR._range.Add(5);
+            _moveR._range.Add(6);
+            _moveR._current = _moveR._range[0];
 
+            _moveL._range = new List<int>();
+            _moveL._range.Add(7);
+            _moveL._range.Add(8);
+            _moveL._range.Add(9);
+            _moveL._range.Add(10);
+            _moveL._range.Add(11);
+            _moveL._range.Add(12);
+            _moveL._current = _moveL._range[0];
+
+            _stay._range = new List<int>();
+            _stay._range.Add(13);
+            _stay._range.Add(14);
+            _stay._current = _stay._range[(_direction - 1) >= 0 ? 0 : 1];
+            _animation.CurrentFrame = _stay._current;
+        }
 
         public override void Update(double deltaTime, Microsoft.DirectX.DirectInput.KeyboardState keyState, Microsoft.DirectX.DirectInput.MouseState mouseState)
         {
@@ -28,23 +55,42 @@ namespace TestDirectX2
 
         public override void Move(KeyboardState keyState)
         {
-
             //throw new NotImplementedException();
             //this._direction = direction;
-
+            
             if (Direction == -1)
             {
-                PositionX = PositionX - MoveSpeed; 
+                PositionX = PositionX - MoveSpeed;
+
+                if (_moveL._current < 12)
+                {
+                    _moveL._current++;
+                } if (_moveL._current == 12)
+                {
+                    _moveL._current = _moveL._range[0];
+                }
+                _animation.CurrentFrame = _moveL._current;
+                _stay._current = _stay._range[(_direction - 1) >= 0 ? 0 : 1];
             }
             if (Direction == 1)
             {
                 PositionX = PositionX + MoveSpeed;
+                if (_moveR._current < _moveR._range.Count)
+                {
+                    _moveR._current++;
+                }
+                if (_moveR._current == _moveR._range.Count)
+                {
+                    _moveR._current = _moveR._range[0];
+                }
+                _animation.CurrentFrame = _moveR._current;
+                _stay._current = _stay._range[(_direction - 1) >= 0 ? 0 : 1];
             }
             if (Direction == 0)
             {
                 PositionX = PositionX;
-            }
-
+                _animation.CurrentFrame = _stay._current;
+            } 
         }
         public override void Attack()
         {
@@ -53,6 +99,7 @@ namespace TestDirectX2
         public override void Draw(int x, int y, Surface surface)
         {
             base.Draw(x,y,surface);
+            Animation.Draw(x, y, surface);
         }
     }
 }
