4283cb2ffa09bdaf4bf6b3ced49f062d64e9ee4d
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@76baa962-0c3b-e8cc-c4fd-2fd9930c05a7>
2012-08-22 16:01:02 +0000

    Tạo AnimationPlayer để điều khiển chuyển động

---
+++ TestDirectX2/TestDirectX2/Core/AnimationPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.DirectX.DirectDraw;
+
+namespace TestDirectX2.Core
+{
+    public class AnimationPlayer
+    {
+        private DxAnimation _animation;
+        private AnimationKey _currentKey;
+
+        public void Play(DxAnimation animation)
+        {
+            if (this._animation != animation)
+            {
+                this._animation = animation;
+            }
+        }
+
+        public void PlayKey(AnimationKey key)
+        {
+            if (this._currentKey != key)
+            {
+                this._currentKey = key;
+                this._animation.CurrentKey = this._currentKey;
+                this._animation.CurrentFrame = this._currentKey._range.First();
+                this._animation.IsLoop = this._currentKey._isLoop;
+            }
+        }
+
+
+        public void Update(float deltaTime)
+        {
+            _animation.Update(deltaTime);
+        }
+
+        public void Draw(int x, int y, Surface dest)
+        {
+            _animation.Draw(x, y, dest);
+        }
+    }
+}
