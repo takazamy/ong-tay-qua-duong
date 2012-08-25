1355390fee3c491cdc39bae595ddb2c083e38aae
tuquoctrungx@gmail.com <tuquoctrungx@gmail.com@76baa962-0c3b-e8cc-c4fd-2fd9930c05a7>
2012-08-25 14:14:55 +0000

    them SoundManager.cs

---
+++ trunk/TestDirectX2/TestDirectX2/SoundManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.DirectX.DirectSound;
+using Microsoft.DirectX.AudioVideoPlayback;
+
+namespace TestDirectX2
+{
+    class SoundManager
+    {
+        Audio music = new Audio("intro.wav");
+        music = new Audio("intro.wav",true);
+    }
+}
