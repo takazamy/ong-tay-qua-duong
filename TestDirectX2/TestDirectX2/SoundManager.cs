43f0cc0dd528d95851135b89552205d4548b435f
nguyendoanphu2310@gmail.com <nguyendoanphu2310@gmail.com@76baa962-0c3b-e8cc-c4fd-2fd9930c05a7>
2012-09-02 08:59:13 +0000

    Update SoundManager.cs (vẫn chưa hoàn chỉnh)

--- TestDirectX2/TestDirectX2/SoundManager.cs
+++ TestDirectX2/TestDirectX2/SoundManager.cs
@@ -10,6 +10,6 @@ namespace TestDirectX2
     class SoundManager
     {
         Audio music = new Audio("intro.wav");
-        music = new Audio("intro.wav",true);
+       // music = new Audio("intro.wav",true);
     }
 }
